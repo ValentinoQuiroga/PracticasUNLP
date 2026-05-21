4f8b8ff4b4de70d90f3beb7b13c36dbe0f86551d
ValentinoQuiroga <valentinoqga@gmail.com>
2026-05-20 23:32:06 -0300

    Continuacion con ejercicios Rust

--- Rust/SeminarioRust/src/main.rs
+++ Rust/SeminarioRust/src/main.rs
@@ -1,32 +1,8 @@
 mod tp02;
+mod tp03;
+use tp03::ej01::Persona;
 
 fn main() {
-    
+    let a: Persona = Persona::new("Valen".to_string(), 24, Some("Calle 10".to_string()));   
+    println!("{}", a.to_string()); 
 }
-
-#[test]
-    fn es_numero_par(){
-        assert_eq!(tp02::ej1::es_par(6), true);
-    }
-#[test]
-    fn no_es_numero_par(){
-        assert_eq!(tp02::ej1::es_par(7), false);
-    }
-#[test]
-    fn es_numero_primo(){
-        assert_eq!(tp02::ej2::es_primo(23), true);
-    }
-#[test]
-    fn no_es_numero_primo(){
-        assert_eq!(tp02::ej2::es_primo(24), false);
-    }
-#[should_panic]
-#[test]
-    fn panic_es_primo(){
-        tp02::ej2::es_primo(-1);
-    }
-#[test]
-    fn suma_pares_arrelo(){
-        let arreglo = [1,2,3,4,5];
-        assert_eq!(tp02::ej3::suma_pares(arreglo),6);
-    }
--- Rust/SeminarioRust/src/tp03/ej01.rs
+++ Rust/SeminarioRust/src/tp03/ej01.rs
@@ -1,13 +1,24 @@
+use std::ops::Add;
 
-struct Persona{
+
+pub struct Persona{
     nombre: String,
-    edad: int,
-    direccion: Optional<String>
+    edad: u32,
+    direccion: Option<String>
 }
 
 impl Persona{
-    fn new(nombre: String, edad:int, direccion: Optional<String>) -> Persona{
+    pub fn new(nombre: String, edad:u32, direccion: Option<String>) -> Persona{
         Persona{nombre,edad,direccion}
+    }
+
+    pub fn to_string(&self) -> String{
+        let mut texto: String = self.nombre.to_string() + " " + &self.edad.to_string() + " ";
+        match &self.direccion{
+            Some(valor) => {texto = texto + &valor},
+            _ => ()
+        }
+        texto 
     }
 }
 
@@ -18,9 +29,17 @@ mod tests{
     use super::*;
     #[test]
     fn crearPersonaCorrectamente(){
-        let persona1 = Persona.new("Valentino", 24);
-        assert_eq(persona1.nombre, "Valentino");
-        assert_eq(persona1.edad, 24);
-        assert_eq(persona1.direccion, None); 
+        let persona1 = Persona::new("Valentino".to_string(), 24, None);
+        assert_eq!(persona1.nombre, "Valentino");
+        assert_eq!(persona1.edad, 24);
+        assert_eq!(persona1.direccion, None); 
+    }
+
+    #[test]
+    fn crearString(){
+        let persona1 = Persona::new("Valentino".to_string(), 24, None);
+        println!("{}", persona1.to_string());
+        let texto: &str = "Valentino24";
+        assert_eq!(persona1.to_string(), texto);
     }
 }
